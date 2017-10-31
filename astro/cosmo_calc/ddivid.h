47c7f81c160adf8b60048b36c19af236110a3060
Aaron LI <aly@aaronly.me>
2017-10-31 16:32:33 +0800

    Remove astro/cosmo_calc: replaced by chandra-acis-analysis/bin/cosmo_calc.py

--- astro/cosmo_calc/ddivid.h
+++
@@ -1,8 +0,0 @@
-#ifndef DDIVID
-#define DDIVID
-
-extern double ddivid(double (*foo)(double),double x1,double x2,double err);
-extern double ddivid(double (*foo)(double),double z,double x1,double x2,double err);
-
-#endif
-//EOF
