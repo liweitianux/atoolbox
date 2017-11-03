e148cb4ec090da55cfc3f00dc1631900e48e0892
Aaron LI <aly@aaronly.me>
2017-11-03 18:51:22 +0800

    Update termcolors1 to not depend on bash; update termcolors2 a bit

--- unix/termcolors2.bash
+++ unix/termcolors2.bash
@@ -1,32 +1,48 @@
 #!/usr/bin/env bash
-# Original: http://frexx.de/xterm-256-notes/
-#           http://frexx.de/xterm-256-notes/data/colortable16.sh
-# Modified by Aaron Griffin
-# and further by Kazuo Teramoto
+#
+# Credits:
+# [1] http://frexx.de/xterm-256-notes/data/colortable16.sh
+# [2] https://wiki.archlinux.org/index.php/X_resources
+#
+# Modified by Aaron Griffin, and Kazuo Teramoto
+#
+
+
 FGNAMES=(' black ' '  red  ' ' green ' ' yellow' '  blue ' 'magenta' '  cyan ' ' white ')
 BGNAMES=('DFT' 'BLK' 'RED' 'GRN' 'YEL' 'BLU' 'MAG' 'CYN' 'WHT')
-
+#echo "     ----------------------------------------------------------------------------"
 echo "     ┌──────────────────────────────────────────────────────────────────────────┐"
-for b in {0..8}; do
-  ((b>0)) && bg=$((b+39))
+for b in $(seq 0 8); do
+    if [ "$b" -gt 0 ]; then
+        bg=$(($b+39))
+    fi
 
-  echo -en "\033[0m ${BGNAMES[b]} │ "
-  
-  for f in {0..7}; do
-    echo -en "\033[${bg}m\033[$((f+30))m ${FGNAMES[f]} "
-  done
-  
-  echo -en "\033[0m │"
-  echo -en "\033[0m\n\033[0m     │ "
-  
-  for f in {0..7}; do
-    echo -en "\033[${bg}m\033[1;$((f+30))m ${FGNAMES[f]} "
-  done
+    #echo -en "\033[0m ${BGNAMES[$b]} : "
+    echo -en "\033[0m ${BGNAMES[$b]} │ "
 
-  echo -en "\033[0m │"
-  echo -e "\033[0m"
+    for f in $(seq 0 7); do
+        fg=$(($f+30))
+        echo -en "\033[${bg}m\033[${fg}m ${FGNAMES[$f]} "
+    done
 
-  ((b<8)) &&
-  echo "     ├──────────────────────────────────────────────────────────────────────────┤"
+    #echo -en "\033[0m :"
+    #echo -en "\033[0m\n\033[0m     : "
+    echo -en "\033[0m │"
+    echo -en "\033[0m\n\033[0m     │ "
+
+    for f in $(seq 0 7); do
+        fg=$(($f+30))
+        echo -en "\033[${bg}m\033[1;${fg}m ${FGNAMES[$f]} "
+    done
+
+    #echo -en "\033[0m :"
+    echo -en "\033[0m │"
+    echo -e "\033[0m"
+
+  if [ "$b" -lt 8 ]; then
+    #echo "     ----------------------------------------------------------------------------"
+    echo "     ├──────────────────────────────────────────────────────────────────────────┤"
+  fi
 done
+#echo "     ----------------------------------------------------------------------------"
 echo "     └──────────────────────────────────────────────────────────────────────────┘"
